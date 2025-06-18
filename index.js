a99b6cf0f9c6b8e1166fc1f220173002af4779a2
otavioaugustos <otavioaugustosnog@gmail.com>
2025-06-18 17:49:21 +0000

    Aula_18_06

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'hbs')
+app.set('views','./views')
+
+app.use(express.static('public'))
+
+app.get('/', (request, response) => {
+    response.render('home');
+});
+
+
+app.listen(port, () => {
+    console.log("Aplicação em execução na porta " +port);
+});
